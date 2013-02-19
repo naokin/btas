dfd74a9afa84fad86afa79985f3e2afca0e1d260
naokin <n.nakatani0608@gmail.com>
2013-02-18 22:50:19 -0500

    compile option -DMKL_CLBAS was disabled until blas interface is fully implemented

--- lib/libbtas.C
+++ lib/libbtas.C
@@ -11,12 +11,12 @@ int TinyVector<int, 0>::m_dummy_data = 0;
 namespace btas
 {
 
-#ifdef MKL_CBLAS
+//#ifdef MKL_CBLAS
 const BTAS_TRANSPOSE Trans     = CblasTrans;
 const BTAS_TRANSPOSE NoTrans   = CblasNoTrans;
 const BTAS_TRANSPOSE ConjTrans = CblasConjTrans;
 const BTAS_ORDER     RowMajor  = CblasRowMajor;
 const BTAS_ORDER     ColMajor  = CblasColMajor;
-#endif
+//#endif
 
 };
