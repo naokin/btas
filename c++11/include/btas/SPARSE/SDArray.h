52a8e5c11313915fb196cf1d716767c18ed3a202
naokin <n.nakatani0608@gmail.com>
2013-07-31 14:04:46 -0400

    Changed makefile for C++11

--- c++11/include/btas/SPARSE/SDArray.h
+++ c++11/include/btas/SPARSE/SDArray.h
@@ -5,12 +5,19 @@
 #include <btas/TVector.h>
 
 #include <btas/SPARSE/STArray.h>
+#include <btas/SPARSE/STdsum.h>
 
 namespace btas {
 
 //! alias to double precision real sparse-array
 template<size_t N>
 using SDArray = STArray<double, N>;
+
+template<size_t N>
+inline void SDdsum(const SDArray<N>& x, const SDArray<N>& y, SDArray<N>& z) { STdsum(x, y, z); }
+
+template<size_t N, size_t K>
+inline void SDdsum(const SDArray<N>& x, const SDArray<N>& y, const IVector<K>& trace_index, SDArray<N>& z) { STdsum(x, y, trace_index, z); }
 
 }; // namespace btas
 
