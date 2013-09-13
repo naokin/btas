369c1705af555d43d4d7a6fd93784e1a7d6ea5ba
Brecht Verstichel <brecht.verstichel@ugent.be>
2013-09-13 14:32:22 -0400

    added documentation

--- include/btas/QSPARSE/QSDArray.h
+++ include/btas/QSPARSE/QSDArray.h
@@ -10,19 +10,19 @@
  *
  * \section FEATURES
  *
- * 1. Provided generic type array classes, TArray<T, N>, STArray<T, N>, and QSTArray<T, N, Q = Quantum>
+ *  - Provides generic type array classes, TArray<T, N>, STArray<T, N>, and QSTArray<T, N, Q = Quantum>
  * 
- * 2. Defined DArray<N> as the alias to TArray<double, N> (using template alias in C++11). SDArray<N> and QSDArray<N, Q> as well
+ *  - Defines DArray<N> as the alias to TArray<double, N> (using template alias in C++11). SDArray<N> and QSDArray<N, Q> as well
  * 
- * 3. Provided LAPACK interfaces written in C
+ *  - Provides LAPACK interfaces written in C
  * 
- * 4. Provided BLAS/LAPACK-like interfaces called with DArray<N>, STArray<N>, and QSTArray<N, Q>
+ *  - Provides BLAS/LAPACK-like interfaces called with DArray<N>, STArray<N>, and QSTArray<N, Q>
  * 
- * 5. Provided expressive contraction, permutation, and decomposition functions
+ *  - Provides expressive contraction, permutation, and decomposition functions
  *
  * \section COMPILATION
  *
- * 1. Compiler and Library Dependencies
+ *  -Compiler and Library Dependencies
  * 
  * GNU GCC 4.7.0 or later
  * Intel C/C++ Compiler 13.0 or later
@@ -30,12 +30,12 @@
  * BOOST library (<http://www.boost.org/>)
  * CBLAS & LAPACK library or Intel MKL library
  *
- * 2. Build libbtas.a
+ *  - Build libbtas.a
  *
  *    cd $BTAS_ROOT/lib/
  *    make
  *
- * 3. Build your code with BTAS library (GCC with MKL library)
+ *  - Build your code with BTAS library (GCC with MKL library)
  *
  *    g++ -std=c++0x -O3 -fopenmp -I$BTAS_ROOT/include $BTAS_ROOT/lib/libbtas.a -lboost_serialization -lmkl_core -lmkl_intel_lp64 -lmkl_sequential
  *
